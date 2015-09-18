c0ea558f9ab52dc8d307da0ae58a87eb28d44e15
jasondearborn <jasondearborn@gmail.com>
2015-09-18 12:13:13 -0700

    1

--- PUSH-config-change-using-PyEZ-master/push-config-change-pyez.py
+++ PUSH-config-change-using-PyEZ-master/push-config-change-pyez.py
@@ -1,8 +1,9 @@
 from jnpr.junos import Device
-dev = Device('10.8.241.31')
+import sys
+dev = Device(host="10.8.241.31", user="jdearborn", passwd="zRGRcwy5")
 from jnpr.junos.utils.config import Config
 cu = Config(dev)
 dev.open()
 rsp = cu.load( path="config-example.conf" )
 cu.commit()
-
+dev.close()
